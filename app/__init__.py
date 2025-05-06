a4e6540e9eea8a3125e07cf9fba87a84920cebaf
Canaan Guo <jyguojianan@gmail.com>
2025-05-06 11:13:09 +0800

    Refactor to Flask-migration
    Refactor application initialization and database models;
    Add migration scripts for user and share models;
    Removed db from sync

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,34 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
+db = SQLAlchemy()
 application = Flask(__name__)
 
 # Set the secret key used by Flask sessions
-application.config['SECRET_KEY'] = 'GROUP3_BEST_TEAM'  # Replace with a secure key
 csrf = CSRFProtect(application)
 
+
+# sqllite database path folder
+os.makedirs('../instance/', exist_ok=True)
+# Load configuration from config.py in the same folder
+application.config.from_pyfile('config.py')
+
+# Initialize the database with the Flask application
+db.init_app(application) 
+
+import app.models
+
+migrate = Migrate(application, db)
+
 import app.routes              
-from app.models import db
 from app.auth_routes import auth_bp
 from app.upload_routes import upload_bp    
 from app.share import share_bp
 from app.api_routes import api_bp
-import os                                  
-
-# sqllite database path folder
-os.makedirs('../instance/', exist_ok=True)
-
-# Load configuration from config.py in the same folder
-application.config.from_pyfile('config.py')
-
-db.init_app(application)  # Initialize db
-migrate = Migrate(application, db)
+                             
 
 # Register route blueprints 
 application.register_blueprint(auth_bp)
@@ -32,7 +36,4 @@ application.register_blueprint(upload_bp)
 application.register_blueprint(share_bp)
 application.register_blueprint(api_bp)
 
-# Create tables in the database (only if they don't already exist)
-with application.app_context():
-    db.create_all()
 
